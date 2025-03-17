50a5643b6f0c0618d2ca54b2bbbce30535e92a21
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2025-03-16 23:40:37 -0300

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,101 +1,36 @@
 import './index.css'
 import { Canvas } from "@react-three/fiber"
-import { OrbitControls, Text } from "@react-three/drei"
+import { OrbitControls } from "@react-three/drei"
+import { Street } from './components/box'
 
+function BlockGroupV2() {
+  const streetIds = "KJIHGFEDCBA".split("");
+  const totalWidth = streetIds.length * 20;
+  const centerOffset = totalWidth / 2 - 10; // Adjust by half the width minus half a street width
+
+  return (
+    <group position={[-centerOffset, 0, 0]}>
+      {streetIds.map((id, index) => (
+        <Street key={id} x={index * 20} y={0} z={0} streetid={id} />
+      ))}
+    </group>
+  )
+}
 function App() {
-  const render = (n: number) => {
-    const color = () => {
-      // random
-      if (Math.random() > 0.5) {
-        return "#15BC06"
-      }
-      return "#ff4444"
-    }
-    // "#4444ff"
-    return (
-      <>
-        {
-          new Array(1).fill(0).map( // width
-            (_, i) => (
-              new Array(3).fill(0).map( // height
-                (_, j) => (
-                  <>
-                    {
-                      new Array(4).fill(0).map( // depth
-                        (_, k) => (
-                          <group position={[n + i * 5, j * 5, k * 5]} key={i}>
-                            <mesh castShadow>
-                              <boxGeometry args={[4, 4, 4]} />
-                              <meshStandardMaterial color={color()} roughness={1} />
-                            </mesh>
-                          </group>
-                        )
-                      )
-                    }
-
-                  </>
-                )
-              )
-            )
-          )
-        }
-
-        {
-          new Array(1).fill(0).map( // width
-            (_, i) => (
-              new Array(3).fill(0).map( // height
-                (_, j) => (
-                  <>
-                    <Text position={[0, j * 15, 0]} fontSize={2} color="black">
-                      RA
-                    </Text>
-                    {
-                      new Array(4).fill(0).map( // depth
-                        (_, k) => (
-                          <group position={[n + i * 5, 18 + (j * 8), k * 5]} key={i}>
-                            <mesh castShadow>
-                              <boxGeometry args={[4, 4, 4]} />
-                              <meshStandardMaterial color={color()} roughness={1} />
-                            </mesh>
-                            <Text position={[0, 0, 0.6]} fontSize={2} color="black">
-                              {i}, {j}, {k}
-                            </Text>
-                          </group>
-                        )
-                      )
-                    }
-                  </>
-                )
-              )
-            )
-          )
-        }
-      </>
-    )
-  }
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
       <div className="w-full h-full">
-        <Canvas camera={{ position: [10, 10, 10], fov: 50 }}>
+        <Canvas camera={{ position: [0, 65, 100], fov: 75 }}>
           <ambientLight intensity={0.5} />
           <pointLight position={[10, 10, 10]} intensity={1} />
-          {
-            new Array(11).fill(0).map(
-              (_, i) => (
-                <>
-                  {render(i * 32)}
-                </>
-              )
-            )
-          }
-
-          {/*<gridHelper args={[20, 20]} />*/}
-          <OrbitControls makeDefault autoRotate target={[176, 10, 0]} />
+          <BlockGroupV2 />
+          <gridHelper args={[300, 40, "#E2E8F0", "#E2E8F0"]} />
+          <OrbitControls makeDefault target={[0, 4, 0]} />
         </Canvas>
       </div>
-
     </div>
   )
 }
 
+// colocar camera centralizado com armazem na horizontal
 export default App
---
+++ src/components/box.tsx
@@ -0,0 +1,122 @@
+import { Text } from "@react-three/drei"
+import { useFrame } from "@react-three/fiber"
+import { useRef, useState } from "react"
+import { BoxGeometry, Mesh } from "three"
+
+interface boxprops {
+  title: string
+  x: number
+  y: number
+  z: number
+  numlines?: number
+  numposperline?: number
+  boxid: number
+  fillpct: number
+}
+
+export function Box(props: boxprops) {
+  const numlines = props.numlines || 1;
+  const numposperline = props.numposperline || 1;
+  const totalBoxes = numlines * numposperline;
+
+  // Calculate the number of red blocks based on fillpct
+  const redBlocksCount = Math.floor((props.fillpct / 100) * totalBoxes);
+  const blockColors = new Array(totalBoxes).fill("green");
+
+  // Randomly assign red color to some blocks
+  for (let i = 0; i < redBlocksCount; i++) {
+    let randomIndex;
+    do {
+      randomIndex = Math.floor(Math.random() * totalBoxes);
+    } while (blockColors[randomIndex] === "red");
+    blockColors[randomIndex] = "red";
+  }
+
+  return (
+    <group position={[props.x, props.y, props.z]} key={`${props.x}-${props.y}${props.z}`}>
+      {
+        new Array(totalBoxes).fill(0).map((_, index) => {
+          const lineIndex = Math.floor(index / numposperline) + 1; // Line number starts from 1
+          const posIndex = (index % numposperline) + 1; // Position number starts from 1
+          return (
+            <mesh key={index} position={[
+              0,             // z position remains constant
+              posIndex * 5,  // posIndex affects the x position
+              lineIndex * -5, // Now lineIndex affects the y position
+            ]} castShadow>
+              <boxGeometry args={[4, 4, 4]} />
+              <meshStandardMaterial color={blockColors[index]} roughness={1} />
+              <Text position={[2.25, 0, 0]} fontSize={1} color="#CBD5E1" rotation={[0, Math.PI / 2, 0]}>
+                {`L${lineIndex}-P${posIndex}`}
+              </Text>
+              <Text position={[-2.25, 0, 0]} fontSize={1} color="#CBD5E1" rotation={[0, -Math.PI / 2, 0]}>
+                {`L${lineIndex}-P${posIndex}`}
+              </Text>
+              {index === 0 && (
+                <Text position={[0, 0, 2.25]} fontSize={2} color="#CBD5E1" rotation={[0, 0, 0]}>
+                  {`B0${props.boxid}`}
+                </Text>
+              )}
+            </mesh>
+          );
+        })
+      }
+    </group>
+  )
+}
+
+
+interface streetprops {
+  streetid: string
+  x: number
+  y: number
+  z: number
+}
+
+export function Street(props: streetprops) {
+  const textRef = useRef<Mesh>(null);
+  const [hovered, setHovered] = useState(false);
+
+  useFrame(({ camera }) => {
+    if (textRef.current) {
+      // Calculate the horizontal angle between the camera and the text
+      const angle = Math.atan2(
+        camera.position.x - props.x,
+        camera.position.z - 0
+      )
+      textRef.current.rotation.y = angle
+    }
+  })
+
+  return (
+    <group
+      onPointerOver={() => setHovered(true)}
+      onPointerOut={() => setHovered(false)}
+    >
+      <lineSegments
+        position={[props.x, props.y + 21, props.z - 13]}
+        scale={[
+          1.1,
+          1.1,
+          1.1,
+        ]}>
+        <edgesGeometry args={[
+          new BoxGeometry(
+            5,
+            35,
+            20,
+          )]} />
+        <lineBasicMaterial
+          color={hovered ? "#FF0000" : "#94A3B8"}
+        />
+      </lineSegments>
+      <Text ref={textRef} position={[props.x, props.y + 18 + (7 * 4), -10]} fontSize={5} color="black">
+        {`R${props.streetid}`}
+      </Text>
+      <Box x={props.x} y={props.y + 18 + 7 + 7} z={0} title={'teste'} numlines={4} numposperline={1} boxid={4} fillpct={3} />
+      <Box x={props.x} y={props.y + 18 + 7} z={0} title={'teste'} numlines={4} numposperline={1} boxid={3} fillpct={25} />
+      <Box x={props.x} y={props.y + 18} z={0} title={'teste'} numlines={4} numposperline={1} boxid={2} fillpct={75} />
+      <Box x={props.x} y={props.y + 0} z={0} title={'teste'} numlines={4} numposperline={3} boxid={1} fillpct={100} />
+    </group>
+  )
+}
